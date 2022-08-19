3a42ecd303ffcca1f4c540d1f77271f48afb4a88
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-08-19 15:41:07 +0100

    Dummy postgres connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,11 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
