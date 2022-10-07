ccfb0b64d3080c2d665cf5f07bed8512f6e42da1
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-10-07 19:01:57 +0100

    Remove facebook and google oauth providers

--- src/main/kotlin/com/bendouthwaite/portfolioapi/security/oauth2/user/OAuth2UserInfoFactory.kt
+++ src/main/kotlin/com/bendouthwaite/portfolioapi/security/oauth2/user/OAuth2UserInfoFactory.kt
@@ -6,18 +6,6 @@ import com.bendouthwaite.portfolioapi.model.AuthProvider
 object OAuth2UserInfoFactory {
     fun getOAuth2UserInfo(registrationId: String, attributes: Map<String?, Any?>?): OAuth2UserInfo {
         return if (registrationId.equals(
-                AuthProvider.google.toString(),
-                ignoreCase = true
-            )
-        ) {
-            GoogleOAuth2UserInfo(attributes)
-        } else if (registrationId.equals(
-                AuthProvider.facebook.toString(),
-                ignoreCase = true
-            )
-        ) {
-            FacebookOAuth2UserInfo(attributes)
-        } else if (registrationId.equals(
                 AuthProvider.github.toString(),
                 ignoreCase = true
             )
