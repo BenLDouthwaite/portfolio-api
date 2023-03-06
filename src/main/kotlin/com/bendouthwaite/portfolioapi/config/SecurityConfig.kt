8cc3773e047c98319d8badaf2938b47447550357
BLD <Ben.L.Douthwaite@gmail.com>
2023-03-06 21:59:57 +0000

    Simplify setup, remove DB and use env vars to setup single user for auth to test deployment

--- src/main/kotlin/com/bendouthwaite/portfolioapi/config/SecurityConfig.kt
+++ src/main/kotlin/com/bendouthwaite/portfolioapi/config/SecurityConfig.kt
@@ -1,30 +1,24 @@
-package com.bendouthwaite.portfolioapi.config
-
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
-
-
-    // TODO Need to actually test auth being triggered from localhost, / the frontend app.
+    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        // @formatter:off
-        http
-            // TODO Review CORS config
-            // https://stackoverflow.com/questions/36968963/how-to-configure-cors-in-a-spring-boot-spring-security-application
-            .cors().configurationSource { request -> CorsConfiguration().applyPermitDefaultValues() }
-            .and()
-            .authorizeRequests { a ->
-                a
-                    .antMatchers("/", "/error", "/webjars/**", "/any-user").permitAll()
-                    .anyRequest().authenticated()
-            }
-
-            .oauth2Login()
+        http.authorizeRequests()
+            .antMatchers("/api/**").authenticated()
+            .and().httpBasic()
     }
 
-
+    @Throws(Exception::class)
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+            .withUser("username")
+            .password("password")
+            .roles("USER")
+    }
 }
--- src/main/kotlin/com/bendouthwaite/portfolioapi/controller/TaskController.kt
+++ src/main/kotlin/com/bendouthwaite/portfolioapi/controller/TaskController.kt
@@ -1,5 +1,6 @@
 package com.bendouthwaite.portfolioapi.controller
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 class TaskController {
 
     @GetMapping("/tasks")
+    @PreAuthorize("hasRole('USER')")
     fun taskList(): List<String> {
         return listOf("task 1", "task 2")
     }
